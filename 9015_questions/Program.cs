981e9d25c6f48b7f9f23914aa0887ec81798b202
GideonYuval <72265313+giladyuval@users.noreply.github.com>
2024-10-17 15:45:08 +0300

    Add project files.

---
+++ 9015_questions/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9015_questions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //1
+        //implement a recursive function that returns true if all digits of integer n are even, false otherwise
+        //follow:
+        //AllEven(246)
+        //AllEven(138)
+        static bool AllEven(int n)
+        {
+            return false;
+        }
+
+        //2
+        //implement a recursive function that returns true if digit d is in integer n, false otherwise
+        //follow:
+        //IsDigitInNumber(12345,3)
+        //IsDigitInNumber(12345,7)
+        static bool IsDigitInNumber(int n, int d)
+        {
+            return false;
+        }
+
+        //3
+        //follow for:
+        //WhatDoes1(10, 25, 1)
+        //WhatDoes1(100, 105, 0)
+        //WhatDoes1(200, 209, 2)
+        //what does WhatDoes1 do?
+        static int WhatDoes1(int n)
+        {
+            return 0;
+        }
+
+        //4
+        //follow for:
+        //WhatDoes2(12, 15)
+        //WhatDoes2(20, 22)
+        //WhatDoes2(8, 8)
+        //what does WhatDoes2 do?
+        static int WhatDoes2(int a, int b)
+        {
+            return 0;
+        }
+
+
+    }
+}
